808b8caea978993dc2cba4324fcf17104b683d1b
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-12-12 15:35:36 -0600

    Initial client code.

--- public/index.ts
+++ public/index.ts
@@ -1,24 +1,14 @@
 import { io } from "socket.io-client";
+import {Board_State} from "../util/GameData";;
+import Client from "./Client";
+
+const socket = io();
 
 window.onload = () => {
-    const socket = io();
-
-    const form = document.getElementById('form');
-    const input: HTMLInputElement = document.getElementById('input') as HTMLInputElement;
-
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        if (input.value) {
-            socket.emit('chat message', input.value);
-            input.value = '';
-        }
-    });
-
-    const messages = document.getElementById('messages');
-    socket.on('chat message', (msg) => {
-        const item = document.createElement('li');
-        item.textContent = msg;
-        messages.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-    });
+    const c = new Client(socket);
+    document.body.appendChild(c);
 }
+
+socket.on("Update", (message:Board_State)=>{
+    console.log(message);
+});
