e9634a508afcd42965a010b378dc4e5d33abaa93
Alex M <ZimTKE@gmail.com>
2026-05-29 14:46:35 -0500

    Added default sizes and load currently selected size if no current game found.

--- app/Elements/Creater.ts
+++ app/Elements/Creater.ts
@@ -1,5 +1,7 @@
 import {createElement as _, appendContent} from "./util";
 
+const MAX_TILE_SIZE = 20;//px
+
 function n(value:any):number {
     value = Number(value);
     if(isNaN(value))
@@ -13,11 +15,22 @@ function s(value:any):string {
     return String(value);
 }
 
+const STORE_KEY = "minesweeper.settings";
+
 export class GameCreater extends HTMLElement {
     #width  = _("input", {type: "number", id:"numWidth"});
     #height = _("input", {type: "number", id:"numHeight"});
     #count  = _("input", {type: "number", id:"numCount"});
+    #select = _("select", {id:"gameMode"},
+        _("option", {value: JSON.stringify({width:9,  height: 9,  mines:10})}, "Beginner"),
+        _("option", {value: JSON.stringify({width:16, height: 16, mines:40})}, "Intermediate"),
+        _("option", {value: JSON.stringify({width:16, height: 30, mines:99})}, "Expert"),
+        _("option", {value: ""}, "Custom"),
+    )
+    #button = _("button", "New Game");
     #history:Record<string, string> = {};
+    #maxWidth:number;
+    #maxHeight:number;
 
     constructor() {
         super();
@@ -31,14 +44,80 @@ export class GameCreater extends HTMLElement {
         });
 
         this.addEventListener("input", (event)=>{
-            const target:HTMLInputElement|null = event.target as any;
-            if(!target || target.tagName !== "INPUT")
+            const target:HTMLInputElement|HTMLSelectElement|null = event.target as any;
+            if(!target)
                 return;
 
-            if(isNaN(target.value as any)) {
-                target.value = this.#history[target.id];
+            switch(target.tagName) {
+                case "INPUT":
+                    if(isNaN(target.value as any))
+                        target.value = this.#history[target.id];
+
+                    this.__validate();
+                    break;
+
+                case "SELECT":
+                    this.__updateInputs();
+                    
             }
         });
+
+        this.#button.addEventListener("click", ()=>{
+            this.dispatchEvent(new CustomEvent("submit", {
+                bubbles: true
+            }));
+        });
+
+        const cache = localStorage.getItem(STORE_KEY);
+        if(cache)
+            this.#select.value = cache;
+        this.__updateInputs();
+        this.#maxHeight = this.height;
+        this.#maxWidth = this.width;
+    }
+
+    private __updateInputs() {
+        const value = this.#select.value;
+        try {
+            localStorage.setItem(STORE_KEY, value)
+            if(!value)
+                throw null;
+            
+            const data:Minesweeper.State["data"] = JSON.parse(this.#select.value);
+            if(typeof data.width !== "number")
+                throw new TypeError("Width must be a number!");
+            this.width = data.width;
+            this.#width.disabled = true;
+
+            if(typeof data.height !== "number")
+                throw new TypeError("Height must be a number!");
+            this.height = data.height;
+            this.#height.disabled = true;
+
+            if(typeof data.mines !== "number")
+                throw new TypeError("Mine Count must be a number!");
+            this.count = data.mines;
+            this.#count.disabled = true;
+        } catch (e) {
+            if(e)
+                console.error(e);
+
+            this.#width.disabled = false;
+            this.#height.disabled = false;
+            this.#count.disabled = false;
+        }
+    }
+
+    private __validate() {
+        this.width = this.width;
+        this.height = this.height;
+        this.count = this.count;
+    }
+
+    init(maxHeight:number, maxWidth:number) {
+        this.#maxHeight = Math.floor(maxHeight / MAX_TILE_SIZE);
+        this.#maxWidth = Math.floor(maxWidth / MAX_TILE_SIZE);
+        this.__validate();
     }
 
     updateButton(score:number){
@@ -50,7 +129,14 @@ export class GameCreater extends HTMLElement {
     }
 
     set width(value:number) {
-        this.#width.value = s(value);
+        if(isNaN(value) || value < 0) {
+            value = 0;
+        } else if(value < 0) {
+            value 
+        }
+        this.#width.value
+            = this.#history["numWidth"]
+            = s(value);
     }
 
     get height():number {
@@ -58,7 +144,9 @@ export class GameCreater extends HTMLElement {
     }
 
     set height(value:number) {
-        this.#height.value = s(value);
+        this.#height.value
+            = this.#history["numHeight"]
+            = s(value);
     }
 
     get count():number {
@@ -66,26 +154,29 @@ export class GameCreater extends HTMLElement {
     }
 
     set count(value:number) {
-        this.#count.value = s(value);
+        this.#count.value
+            = this.#history["numCount"]
+            = s(value);
     }
 
     connectedCallback(){
         this.innerHTML = "";
 
-        const btnSubmit = _("button", "New Game");
-        btnSubmit.addEventListener("click", ()=>{
-            this.dispatchEvent(new CustomEvent("submit", {
-                bubbles: true
-            }));
-        });
-
         appendContent(this, [
-            _("label", {for: "numWidth"},  "Game Width:"),  this.#width,
-            _("label", {for: "numHeight"}, "Game Height:"), this.#height,
-            _("label", {for: "numCount"},  "Mine Count:"),  this.#count,
-            _("div", {class: "btns"}, btnSubmit)
+            _("div", {class: "row header"}, this.#button, this.#select),
+            _("div", {class: "row input"},
+                _("div", {class:"row"},
+                    _("label", {for: "numWidth"},  "Game Width:"),  this.#width
+                ),
+                _("div", {class:"row"},
+                    _("label", {for: "numHeight"}, "Game Height:"), this.#height
+                ),
+                _("div", {class:"row"},
+                    _("label", {for: "numCount"},  "Mine Count:"),  this.#count
+                )
+            )
         ]);
     }
 }
 
-customElements.define("game-creater", GameCreater)
+customElements.define("game-header", GameCreater)
--- app/index.ts
+++ app/index.ts
@@ -3,11 +3,12 @@ import { apiFetch } from "./Elements/util";
 
 const game = new MinesweeperGame();
 
-apiFetch("load").then((resp)=>{
-    if(resp instanceof Error)
-        return;
+apiFetch("load", game.data).then((resp)=>{
+    if(resp instanceof Error) 
+        return alert(resp.message);
+
     game.init(resp);
-});
+}).catch(console.error);
 
 window.onload = () => {
     history.replaceState(history.state, "", "/");
